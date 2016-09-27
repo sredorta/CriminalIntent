850e8651a2e1084f641e8b345ce5426a6028c543
sredorta <sergi.redorta@hotmail.com>
2016-09-27 11:40:37 +0200

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,57 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+import android.text.format.DateFormat;
+
+/**
+ * Created by sredorta on 9/19/2016.
+ */
+
+public class Crime {
+    private String mTitle;
+    private UUID mId;
+    private Date mDate;
+    private boolean mSolved;
+
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+
+
+    public String getTitle() {
+
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        this.mTitle = title;
+    }
+
+
+
+}
